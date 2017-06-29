566df70581d44aa662255f21513f2874c8c345b2
alexthkim <atk3@rice.edu>
2017-06-29 15:57:43 -0700

    updated request process

--- find.js
+++ find.js
@@ -6,7 +6,8 @@ var program = require('commander');
 var request = require("request");
 var cheerio = require("cheerio");
 var models = require('./models/models');
-var Menu = models.Menu;
+
+var client = require('twilio')(process.env.TWILIO_SID, process.env.TWILIO_AUTH_TOKEN)
 
 // mongoose.connection.on('connected', function() {
 //   console.log('Success: connected to MongoDb!');
@@ -16,28 +17,6 @@ var Menu = models.Menu;
 //   process.exit(1);
 // });
 // mongoose.connect(process.env.MONGODB_URI);
-
-
-// exports.textInput = function (req, res) {
-//     var text = req.query.data;
-//     console.log(text);
-//     fs.writeFile('./public/files/text.txt', text, function (err) {
-//         if (err) {
-//             return console.log('there is an error');
-//         }
-//
-//         console.log('the file was saved');
-//         respondWithFile();
-//     });
-//
-//     function respondWithFile() {
-//         console.log('sending file...');
-//         res.download('./public/files/text.txt');
-//
-//     };
-// };
-
-
 
 var app = express();
 
@@ -68,50 +47,3 @@ function show() {
     });
   });
 }
-
-
-
-// function show() {
-//   console.log("hello");
-//   httpGetAsync("http://dining.rice.edu", function (response) {
-//     const $ = cheerio.load(response);
-//     //var stuff = $('.owl-stage-outer').toArray();
-//     console.log($.html('.owl-stage-outer'));
-
-
-    // var newMenu = new menu ({
-    //   university: "Rice",
-    //   menu: response
-    // })
-    // newMenu.save(function(err) {
-    //   if (err) {
-    //     console.log("Errors")
-    //   } else {
-    //     console.log("success");
-    //     //mongoose.connection.close();
-    //   }
-    // })
-  //   const dom = new JSDOM(response);
-  //   console.log(response);
-  //   var serveryList = dom.window.document.getElementsByClassName("servery-title");
-  //   for (var a = 0; a < serveryList.length; a++) {
-  //     console.log(serveryList[a].textContent);
-  //})
-  // Menu.findOne({"university":"Rice"}, function(err, indvMenu) {
-  //   if (err) {
-  //     console.log("University not found")
-  //   } else {
-  //       const desires = "pasta";
-  //       const dom = new JSDOM(indvMenu);
-  //       var serveryList = dom.window.document.getElementsByClassName("menu-item");
-  //       console.log("You wanted: " + desires);
-  //       for (var a = 0; a < serveryList.length; a++) {
-  //         var menuItem = serveryList[a].textContent;
-  //         if (menuItem.toLowerCase().includes(desires)) {
-  //           var location = serveryList[a].parentNode.parentNode.previousElementSibling.id
-  //           console.log(menuItem + " @ " + location.replace("@", ""));
-  //         }
-  //       }
-  //   }
-  // });
-//}
--- models/models.js
+++ models/models.js
@@ -2,11 +2,38 @@
 
 var mongoose = require('mongoose');
 
-var Menu = mongoose.model('Menu', {
-  university: String,
-  menu: String
-});
+var uniSchema = mongoose.Schema({
+  uni: String,
+  meal: {
+    type: String,
+    enum: ['b', 'l', 'd'],
+    time: Number
+  },
+  locations: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Servery'
+  }]
+})
+
+var serverySchema = mongoose.Schema({
+  servery: String,
+  menuItem: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'MenuItem'
+  }]
+})
+
+var menuItemSchema = mongoose.Schema({
+  item: String,
+  allergies: [String]
+})
+
+var MenuItem = mongoose.model('MenuItem', menuItemSchema);
+var Servery = mongoose.model('Servery', serverySchema);
+var Uni = mongoose.model('Uni', uniSchema);
 
 module.exports = {
-  Menu: Menu
+  Uni: Uni,
+  Servery: Servery,
+  MenuItem: MenuItem
 }
